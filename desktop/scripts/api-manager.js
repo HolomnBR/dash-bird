435c4294610a0c07d7eecedf37706ff794789b27
Lucas Argate <lucas.argate@gmail.com>
2025-09-01 12:32:05 -0300

    foiles

--- desktop/scripts/api-manager.js
+++ desktop/scripts/api-manager.js
@@ -1,9 +1,7 @@
-import { spawn, exec } from 'child_process';
+import { spawn } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import http from 'http';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
