03d6469844609da7aa2ff7e909e794776bede3d9
Lucas Argate <lucas.argate@gmail.com>
2025-09-01 14:37:11 -0300

    salvar API

--- FirebirdApi/Controllers/FirebirdController.cs
+++ FirebirdApi/Controllers/FirebirdController.cs
@@ -3,6 +3,7 @@ using FirebirdApi.Services;
 using System.Data;
 using Swashbuckle.AspNetCore.Annotations;
 
+
 namespace FirebirdApi.Controllers
 {
     [ApiController]
@@ -10,7 +11,7 @@ namespace FirebirdApi.Controllers
     public class FirebirdController : ControllerBase
     {
         private readonly IFirebirdService _firebirdService;
-
+    //teste
         public FirebirdController(IFirebirdService firebirdService)
         {
             _firebirdService = firebirdService;
@@ -22,6 +23,7 @@ namespace FirebirdApi.Controllers
         [ProducesResponseType(typeof(object), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> TestConnection([FromQuery] string? databaseId = null)
         {
+            var teste = "teste";
             try
             {
                 var isConnected = await _firebirdService.TestConnectionAsync(databaseId);
